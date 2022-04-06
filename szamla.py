e3492ab9061280abf119e8a44949c066397c74a8
DávidMarcell <davidmarcell27@gmail.com>
2022-04-06 13:17:39 +0200

    feladat kiosztása

---
+++ szamla.py
@@ -0,0 +1,34 @@
+#1.Feladat(Kiss Kornél)
+
+
+
+
+
+#2.Feladat(Dravicz Levente)
+
+
+
+
+
+#3.Feladat(Dávid Marcell)
+
+
+
+
+
+
+#4.Feladat(Dravicz Levente)
+
+
+
+
+
+
+#5.Feladat(Dávid Marcell)
+
+
+
+
+
+
+#6.Feladat(Dávid Marcell)
